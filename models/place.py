2ea1ed7342bfbfc290427503f53dc6453d4ac3e6
NeneNafe <nenechiwaya@gmail.com>
2023-12-19 23:55:51 +0200

    corrections

--- models/place.py
+++ models/place.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, ForeignKey,String, Integer,Float
+from sqlalchemy import Column, ForeignKey, String, Integer, Float
+from models import hbnb_storage
 from sqlalchemy.orm import relationship
-
+import models
+from uuid import uuid4
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    if hbnb_storage == 'db':
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
 
-    places = relationship('User', backref='places', cascade='all, delete-orphan')
-    places = relationship('City', backref='places',cascade='all, delete-orphan')
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    
+    def __init__(self, **kwargs):
+        self.id = str(uuid4())
+        for key, value in kwargs.items():
+            setattr(self, key, value)
--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from uuid import uuid4
-from models.base_model import BaseModel
-from models.base_model import Base, Column, String
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from models import hbnb_storage
 from sqlalchemy.orm import relationship
 from models.place import Place
@@ -16,7 +16,10 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship('User', backref='places', cascade='all, delete, delete-orphan')
+
+        places = relationship('User', backref='places',
+                              cascade='all, delete, delete-orphan')
+
         def __init__(self, *args, **kwargs):
             self.id = str(uuid4())
             for key, value in kwargs.items():
@@ -26,5 +29,3 @@ class User(BaseModel, Base):
         password = ""
         first_name = ""
         last_name = ""
-    
-    
