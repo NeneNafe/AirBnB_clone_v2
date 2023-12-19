e729deb7ae890da46d2a81479665e92718f67a2d
AzorAha1 <wizfaiz@icloud.com>
2023-12-19 21:17:31 +0100

    update

--- models/place.py
+++ models/place.py
@@ -6,15 +6,15 @@ from sqlalchemy import Column, ForeignKey,String, Integer,Float
 
 class Place(BaseModel, Base):
     """ A place to stay """
+    __tablename__ = 'places',
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        __tablename__ = 'places',
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, default=0, nullable=False)
-        number_bathrooms = Column(Integer, default=0, nullable=False)
-        max_guest = Column(Integer, default=0, nullable=False)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
