83e0fc1b490673bf60a0a756c59b1745a0975ada
AzorAha1 <wizfaiz@icloud.com>
2023-12-19 22:19:41 +0100

    new

--- models/user.py
+++ models/user.py
@@ -11,12 +11,20 @@ from models.place import Place
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    if hbnb_storage == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('User', backref='places', cascade='all, delete, delete-orphan')
+        def __init__(self, *args, **kwargs):
+            self.id = str(uuid4())
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
     
     
-    
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
