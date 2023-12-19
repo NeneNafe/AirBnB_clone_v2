72dce3e65c65ee41624c6d245be735d73d477c6e
NeneNafe <nenechiwaya@gmail.com>
2023-12-19 08:24:05 +0200

    changes

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from uuid import uuid4
 from datetime import datetime
 import models
+import os
 
 Base = declarative_base()
 
@@ -18,34 +19,38 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
 
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-        if kwargs:
-            for key. value in kwargs.items():
-                if key in ['id', 'created_at', 'updated_at']:
-                    setattr(self, key, value)
+        if kwargs == {}:
+            return
+        
+        for key, value in kwargs.items():
+            if key in ['created_at', 'updated_at']:
+                value = datetime.fromisoformat(value)
+            if key != '__class__':
+                self.__dict__[key] = value
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        return '[{}] ({}) {}'.format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        the_dict = self.__dict__.copy()
-        the_dict["__class__"] = str(type(self).__name__)
-        the_dict['created_at'] = self.created_at.isoformat()
-        the_dict['updated_at'] = self.updated_at.isoformat()
-        the_dict.pop("_sa_instance_state", None)
-        return the_dict
+        our_dict = self.__dict__.copy()
+        our_dict['created_at'] = self.created_at.isoformat()
+        our_dict['updated_at'] = self.updated_at.isoformat()
+        our_dict['__class__'] = self.__class__.__name__
+        if '_sa_instance_state' in our_dict.keys():
+            del our_dict['_sa_instance_state']
+        return our_dict
 
     def delete(self):
         """deletes the current instance from the storage"""
