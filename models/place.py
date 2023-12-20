d376ff11845f4bf684c13f9206bf46624b77083e
AzorAha1 <wizfaiz@icloud.com>
2023-12-20 02:06:07 +0100

    update

--- models/place.py
+++ models/place.py
@@ -25,23 +25,23 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
 
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-    places = relationship(
-        'User', backref='places', cascade='all, delete-orphan',
-        single_parent=True
-        )
-    places = relationship(
-        'City', backref='places', cascade='all, delete-orphan',
-        single_parent=True
-        )
+    # city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    # user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    # name = Column(String(128), nullable=False)
+    # description = Column(String(1024), nullable=True)
+    # number_rooms = Column(Integer, default=0, nullable=False)
+    # number_bathrooms = Column(Integer, default=0, nullable=False)
+    # max_guest = Column(Integer, default=0, nullable=False)
+    # latitude = Column(Float, nullable=True)
+    # longitude = Column(Float, nullable=True)
+        places = relationship(
+            'User', backref='places', cascade='all, delete-orphan',
+            single_parent=True
+            )
+    # places = relationship(
+    #     'City', backref='places', cascade='all, delete-orphan',
+    #     single_parent=True
+    #     )
 
     def __init__(self, **kwargs):
         self.id = str(uuid4())
