28322d7fd30b71a031e07812e01bb27536439790
AzorAha1 <wizfaiz@icloud.com>
2023-12-20 02:40:32 +0100

    update

--- models/place.py
+++ models/place.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey,  String, Integer,  Float
 from models import hbnb_storage
@@ -10,8 +12,8 @@ from uuid import uuid4
 
 class Place(BaseModel, Base):
     """ A place to stay"""
+    __tablename__ = 'places'
     if hbnb_storage == 'db':
-        __tablename__ = 'places'
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
@@ -22,24 +24,10 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, default=0, nullable=False)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-
-    # city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    # user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    # name = Column(String(128), nullable=False)
-    # description = Column(String(1024), nullable=True)
-    # number_rooms = Column(Integer, default=0, nullable=False)
-    # number_bathrooms = Column(Integer, default=0, nullable=False)
-    # max_guest = Column(Integer, default=0, nullable=False)
-    # latitude = Column(Float, nullable=True)
-    # longitude = Column(Float, nullable=True)
         places = relationship(
-            'Place', backref='user', cascade='all, delete-orphan',
+            'User', backref='places', cascade='all, delete-orphan',
             single_parent=True
             )
-    # places = relationship(
-    #     'City', backref='places', cascade='all, delete-orphan',
-    #     single_parent=True
-    #     )
 
     def __init__(self, **kwargs):
         self.id = str(uuid4())
