54d09aa11244d36627c40f6792275f76776f533d
AzorAha1 <wizfaiz@icloud.com>
2023-12-20 19:14:15 +0100

    review update

--- models/review.py
+++ models/review.py
@@ -2,20 +2,23 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from models import hbnb_storage
 
 
 class Review(BaseModel):
     """ Review classto store review information """
     __tablename__ = 'reviews'
-    text = Column(String(1024), nullable=False)
-    place_id = Column(
-        String(60), ForeignKey('places.id'),
-        nullable=False
-    )
-    user_id = Column(
-        String(60), ForeignKey('users.id'),
-        nullable=False
+    if hbnb_storage == 'db':
+        text = Column(String(1024), nullable=False)
+        place_id = Column(
+            String(60), ForeignKey('places.id'),
+            nullable=False
         )
-    place_id = ""
-    user_id = ""
-    text = ""
+        user_id = Column(
+            String(60), ForeignKey('users.id'),
+            nullable=False
+            )
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
