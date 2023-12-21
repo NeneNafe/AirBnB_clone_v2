433e5ecf0ab4756c8381623593650efe29c6dc28
AzorAha1 <wizfaiz@icloud.com>
2023-12-21 01:40:20 +0100

    update

--- models/base_model.py
+++ models/base_model.py
@@ -19,20 +19,23 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+            if 'created_at' in kwargs and 'updated_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs.pop['__class__', None]
+            self.__dict__.update(kwargs)
+            self.id = kwargs.get('id', str(uuid.uuid4()))
+            
 
     def __str__(self):
         """str method
