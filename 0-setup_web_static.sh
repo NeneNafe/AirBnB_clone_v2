5d09cbf09f8abe61c088062d9dabe8d0aad7e933
AzorAha1 <wizfaiz@icloud.com>
2024-01-11 12:46:27 +0100

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,18 +3,24 @@
 source=/data/web_static/releases/test/
 symlink=/data/web_static/current
 configcode="location /hbnb_static {
-    alias /data/web_static/current/
-    index index.html
-}"
-sudo apt-get install -y nginx
+    alias /data/web_static/current/;
+    index index.html;
+};"
+configfile=/etc/nginx/sites-enabled/default
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "snippet content" | sudo tee "$source/index.html"
+echo "snippet content" | sudo tee -a "$source/index.html"
 if [ -e "$symlink "]; then
-    rm "$symlink"
+    sudo rm "$symlink"
 fi
+
 sudo ln -s $source $symlink
 sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/server {/a '"$configcode" "$configfile"
