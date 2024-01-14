931642e630ae43e81a896abd50c33dfe7b58e6f3
NeneNafe <nenechiwaya@gmail.com>
2024-01-14 11:35:06 +0200

    Corrections

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,34 +4,50 @@ distributes an archive to your web servers, using the function do_deploy
 """
 
 import os
-from fabric.api import local, put, env, run, sudo
+from fabric.api import *
 from datetime import datetime
+
 
 env.hosts = ['100.26.132.15', '100.25.118.3']
 env.user = 'ubuntu'
 
+
+def do_pack():
+    """
+        return the archive path if archive has generated correctly.
+    """
+
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
+        return None
+
+
 def do_deploy(archive_path):
-    """distributes an archive to webservers"""
-    if not os.path.exists(archive_path):
-        return False
+    """
+        Distribute archive.
+    """
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
 
-    basename = os.path.basename(archive_path)
-    rem_archive_path = f"/tmp/{basename}"
+        print("New version deployed!")
+        return True
 
-    try:
-        put(archive_path, rem_archive_path)
-        x_archive = "/data/web_static/releases/{}".format(
-            os.path.splitext(basename)[0]
-        )
-        run(f"mkdir -p {x_archive}")
-        run("tar -xzf {} -C {} --strip-components=1".format(
-            rem_archive_path, x_archive
-            ))
-        run(f"rm -f {rem_archive_path}")
-        symlink = "/data/web_static/current"
-        run(f"rm -rf {symlink}")
-        run(f"ln -s {x_archive}/ {symlink}")
-    except Exception:
-        return False
-
-    return True
+    return False
