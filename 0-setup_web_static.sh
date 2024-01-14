9c83306c39f0eec81e5060e64e82c08631e06df2
NeneNafe <nenechiwaya@gmail.com>
2024-01-15 00:22:44 +0200

    Corrections

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,23 +12,24 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
 
 printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $hostname;
-    root   /var/www/html;
+    listen 80;
+    listen [::]:80;
+
+    add_header X-Served-By 318591-web-01;
     index  index.html index.htm;
+
     location /hbnb_static {
         alias /data/web_static/current;
-        index index.html index.htm;
+        try_files $uri $uri/ =404;
     }
     location /redirect_me {
         return 301 http://github.com/NeneNafe;
     }
     error_page 404 /404.html;
-    location /404 {
+    location = /404.html {
       root /var/www/html;
       internal;
     }
-}" > /etc/nginx/sites-available/default
+}" > /etc/nginx/sites-enabled/default
 
 service nginx restart
