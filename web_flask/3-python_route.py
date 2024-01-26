7d3fe9049d4abb471462a84d221350547fa80197
NeneNafe <nenechiwaya@gmail.com>
2024-01-26 09:10:47 +0200

    Python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" adds a text that says python is cool"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """returns the string Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """return the string HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def cis_fun(text):
+    """adds a text that says C is fun"""
+    text_with_spaces = text.replace('_', ' ')
+    return 'C {}'.format(text_with_spaces)
+
+
+@app.route('/python/<text>', defaults={'text': 'is cool'})
+def python_is_cool(text):
+    """A text python is cool"""
+    text_with_spaces = text.replace('_', ' ')
+    return 'Python {}'.format(text_with_spaces)
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
